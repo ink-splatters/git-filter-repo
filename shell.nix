8cc757321b491629e9c75b1819560f64b0c2b66c
Peter A. <ink.splatters@pm.me>
2024-08-24 08:15:31 +0200

    refactored shell.nix

--- shell.nix
+++ shell.nix
@@ -1,19 +1,35 @@
 with import <nixpkgs> { };
 let
-  inherit (llvmPackages_18) stdenv libcxx;
+  llvmPackages = llvmPackages_18;
+  clang = clang_18;
+  lld = lld_18;
 in
-mkShell.override { inherit stdenv; } {
-  nativeBuildInputs = [
-    libcxx
-    clang_18
-    lld_18
-    gnumake
-  ];
 
-  shellHook = ''
-    export CXXFLAGS="-isystem ${libcxx.dev}/include/c++/v1 -Wall -O3 -mcpu=apple-m1 -funroll-loops -ffast-math -std=c++20 -stdlib=libc++ -flto=thin"
-    export LDFLAGS="-fuse-ld=lld"
-    export CC=clang++
-  '';
+(
+  {
+    clang,
+    lld,
+    llvmPackages,
+    ...
+  }:
+  let
+    inherit (llvmPackages) stdenv libcxx;
+  in
+  mkShell.override { inherit stdenv; } {
+    nativeBuildInputs = [
+      libcxx
+      clang
+      lld
+      gnumake
+    ];
 
-}
+    shellHook = ''
+      export CXXFLAGS="-isystem ${libcxx.dev}/include/c++/v1 -Wall -O3 -mcpu=apple-m1 -funroll-loops -ffast-math -std=c++20 -stdlib=libc++ -flto=thin"
+      export LDFLAGS="-fuse-ld=lld"
+      export CC=clang++
+    '';
+
+  }
+)
+
+  { inherit clang lld llvmPackages; }
