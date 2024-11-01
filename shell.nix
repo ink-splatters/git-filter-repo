262e850eb258990844b5409476fac39510f1bb3b
Peter A. <ink.splatters@pm.me>
2024-11-01 15:10:22 +0100

    updated: * shell.nix * cythonized code

--- shell.nix
+++ shell.nix
@@ -1,35 +1,18 @@
 with import <nixpkgs> { };
 let
-  llvmPackages = llvmPackages_18;
-  clang = clang_18;
-  lld = lld_18;
+  inherit (llvmPackages_19) libcxxStdenv bintools lld;
+  stdenv = libcxxStdenv;
 in
+mkShell.override { inherit stdenv; } {
+  nativeBuildInputs = [
+    lld
+    gnumake
+  ];
 
-(
-  {
-    clang,
-    lld,
-    llvmPackages,
-    ...
-  }:
-  let
-    inherit (llvmPackages) stdenv libcxx;
-  in
-  mkShell.override { inherit stdenv; } {
-    nativeBuildInputs = [
-      libcxx
-      clang
-      lld
-      gnumake
-    ];
+  shellHook = ''
+    export CXXFLAGS="-Wall -O3 -mcpu=apple-m1 -funroll-loops -ffast-math -std=c++20 -flto"
+    export LDFLAGS="-fuse-ld=lld"
+    export CC=clang++
+  '';
 
-    shellHook = ''
-      export CXXFLAGS="-isystem ${libcxx.dev}/include/c++/v1 -Wall -O3 -mcpu=apple-m1 -funroll-loops -ffast-math -std=c++20 -stdlib=libc++ -flto=thin"
-      export LDFLAGS="-fuse-ld=lld"
-      export CC=clang++
-    '';
-
-  }
-)
-
-  { inherit clang lld llvmPackages; }
+}
